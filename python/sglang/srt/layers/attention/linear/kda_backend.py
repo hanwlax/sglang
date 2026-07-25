49ee9882e3a6714fe499b92c9a9ecb504ac6fa03
zhang-chunli01 <zhangchunli19@huawei.com>
2026-07-25 16:00:09 +0800

    feat(causal_conv1d): use triton conv1d in kda_backend
    Revert commit: feat(causal_conv1d): use triton conv1d in kda_backend
    This revert commit: 791e2f446dee50768a5d2dc343b217415dd4ce64

--- python/sglang/srt/layers/attention/linear/kda_backend.py
+++ python/sglang/srt/layers/attention/linear/kda_backend.py
@@ -454,25 +454,17 @@ class KDAAttnBackend(MambaAttnBackendBase):
 
         conv_states = self._channel_first_conv_states(conv_states)
         if is_npu():
-            qkv = causal_conv1d_update(
-                mixed_qkv,
-                conv_states,
+            qkv, updated_conv_states = torch_causal_conv1d_update_npu(
+                mixed_qkv.unsqueeze(-1),
+                conv_states[cache_indices],
                 layer.conv_weights,
-                layer.bias,
+                bias=layer.bias,
                 activation="silu",
-                conv_state_indices=cache_indices,
             )
-            # qkv, updated_conv_states = torch_causal_conv1d_update_npu(
-            #     mixed_qkv.unsqueeze(-1),
-            #     conv_states[cache_indices],
-            #     layer.conv_weights,
-            #     bias=layer.bias,
-            #     activation="silu",
-            # )
-            # conv_states.index_copy_(
-            #     0, cache_indices, updated_conv_states.to(conv_states.dtype)
-            # )
-            # qkv = qkv.squeeze(-1)
+            conv_states.index_copy_(
+                0, cache_indices, updated_conv_states.to(conv_states.dtype)
+            )
+            qkv = qkv.squeeze(-1)
         else:
             qkv = causal_conv1d_update(
                 mixed_qkv,
